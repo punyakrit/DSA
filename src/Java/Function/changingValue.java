fe2a59dae0482400b15d263dbdd986fe814d9fad
Punyakrit Singh Makhni <punyakritsinghmakhni@gmail.com>
2023-03-02 23:45:50 +0530

    function swaping

---
+++ src/Java/Function/changingValue.java
@@ -0,0 +1,16 @@
+package Java.Function;
+
+public class changingValue {
+    public static void main(String[] args) {
+//        String name = "Anoop";
+//        nam(name);
+//        System.out.println(name);
+    }
+
+
+
+    private static void nam(String naae) {
+        naae = "Abc";
+    }
+
+}
